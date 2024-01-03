9ea90fb550e231e3b23075461397c259cd35852d
xkuadrattt <calmystu.00@gmail.com>
2024-01-03 11:54:32 +0700

    add autopairs

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -1,68 +1,70 @@
 return {
-{ "elentok/format-on-save.nvim" },
-    { 'numToStr/Comment.nvim', opts = {} },
+  { "elentok/format-on-save.nvim" },
+  { 'numToStr/Comment.nvim',      opts = {} },
 
-    {
-        "numToStr/Comment.nvim",
-        config = function()
-            require("Comment").setup()
-        end
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end
+  },
+
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme onedark")
+    end
+  },
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+
+  'folke/neodev.nvim', -- new
+
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      'rafamadriz/friendly-snippets',
+
+      'hrsh7th/cmp-nvim-lsp',
+
     },
+  },
 
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000,
-        config = function()
-            vim.cmd("colorscheme gruvbox")
-        end
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+  },
+
+
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'make',
+  },
+  { 'vijaymarupudi/nvim-fzf' },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
     },
-    --
-    
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-
-    'folke/neodev.nvim', -- new
-
-    {
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
-            'rafamadriz/friendly-snippets',
-            
-            'hrsh7th/cmp-nvim-lsp',
-            
-        },
-    },
-
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-    },
-
-
-    {
-        'nvim-telescope/telescope.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-
-    {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        run = 'make',
-    },
-	{'vijaymarupudi/nvim-fzf'},
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = {
-            "nvim-tree/nvim-web-devicons"
-        },
-        config = function()
-            require("lualine").setup({
-                icons_enabled = true,
-                theme = 'onedark',
-            })
-        end,
-    }
+    config = function()
+      require("lualine").setup({
+        icons_enabled = true,
+        theme = 'onedark',
+      })
+    end,
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+  }
 
 }
